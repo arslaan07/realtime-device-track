0d5bb77d7180ca4e9e98fb3386028168518b6bd7
arslaan07 <arslaanbanday5@gmail.com>
2024-07-24 14:39:05 +0530

    vercel.json added

--- app.js
+++ app.js
@@ -23,4 +23,7 @@ app.get("/", (req, res) => {
     res.render("index")
 })
 
-server.listen(3000)
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
